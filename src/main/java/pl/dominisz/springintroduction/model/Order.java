ee058cff02678ebf2b51ea23b7d8bd336651daaa
alergeek <trojka95@gmail.com>
2019-05-18 11:42:22 +0200

    add OrderItem change Order: add params, constructor, getters

--- src/main/java/pl/dominisz/springintroduction/model/Order.java
+++ src/main/java/pl/dominisz/springintroduction/model/Order.java
@@ -1,13 +1,26 @@
 package pl.dominisz.springintroduction.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Order {
 
+    private List<OrderItem> items;
 
-    private BigDecimal amount;
+    public Order() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(OrderItem orderItem){
+        items.add(orderItem);
+    }
 
     public BigDecimal getAmount() {
-        return amount;
+        BigDecimal sum = BigDecimal.ZERO;
+        for(OrderItem item : items){
+            sum = sum.add(item.getPrice().multiply(new BigDecimal(item.getQuantity())));
+        }
+        return sum;
     }
 }
